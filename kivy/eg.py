ca66e155ac6f6b7cf3cb779de0af858218c49357
Mauray-Jain <maurayjain@gmail.com>
2023-09-08 22:30:15 +0530

    Nothing much

---
+++ kivy/eg.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class MainWidget(Widget):  # Main interface
+    pass
+
+class EgApp(App):
+    pass
+
+EgApp().run()
+
+# Notes:
+# screen lies in 1st quadrant, i.e., (0,0) is lower left
